ba9070e22a4e2dd3a24b511e714505672e4bb300
YukiKimura <yukikimuraqq@gmail.com>
2025-03-21 03:55:34 +0900

    Scene07 add

--- Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
+++ Assets/ModularFirstPersonController/FirstPersonController/FirstPersonController.cs
@@ -210,8 +210,8 @@ public class FirstPersonController : MonoBehaviour
         // Control camera movement
         if(cameraCanMove)
         {
-            JoyH = Input.GetAxis("JoyRightH") / 2;
-            JoyV = Input.GetAxis("JoyRightV") / 5;
+            JoyH = Input.GetAxis("JoyRightH") / 50;
+            JoyV = Input.GetAxis("JoyRightV") / 50;
             //yaw = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * mouseSensitivity;
             yaw = transform.localEulerAngles.y - JoyH * mouseSensitivity;
 
